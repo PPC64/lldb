f6af689a252c492807c573a52720de40a6b293fb
luporl <leandro.lupori@gmail.com>
2018-02-08 14:27:24 -0200

    [LLDB] Initial version of PPC64 InstEmulation

---
+++ source/Plugins/Instruction/PPC64/EmulateInstructionPPC64.h
@@ -0,0 +1,94 @@
+//===-- EmulateInstructionPPC64.h -------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef EmulateInstructionPPC64_h_
+#define EmulateInstructionPPC64_h_
+
+// C Includes
+// C++ Includes
+// Other libraries and framework includes
+// Project includes
+#include "lldb/Core/EmulateInstruction.h"
+#include "lldb/Interpreter/OptionValue.h"
+
+class EmulateInstructionPPC64 : public lldb_private::EmulateInstruction {
+public:
+  EmulateInstructionPPC64(const lldb_private::ArchSpec &arch)
+      : EmulateInstruction(arch) {}
+
+  static void Initialize();
+
+  static void Terminate();
+
+  static lldb_private::ConstString GetPluginNameStatic();
+
+  static const char *GetPluginDescriptionStatic();
+
+  static lldb_private::EmulateInstruction *
+  CreateInstance(const lldb_private::ArchSpec &arch,
+                 lldb_private::InstructionType inst_type);
+
+  static bool SupportsEmulatingInstructionsOfTypeStatic(
+      lldb_private::InstructionType inst_type) {
+    switch (inst_type) {
+    case lldb_private::eInstructionTypeAny:
+    case lldb_private::eInstructionTypePrologueEpilogue:
+      return true;
+
+    case lldb_private::eInstructionTypePCModifying:
+    case lldb_private::eInstructionTypeAll:
+      return false;
+    }
+    return false;
+  }
+
+  lldb_private::ConstString GetPluginName() override;
+
+  uint32_t GetPluginVersion() override { return 1; }
+
+  bool SetTargetTriple(const lldb_private::ArchSpec &arch) override;
+
+  bool SupportsEmulatingInstructionsOfType(
+      lldb_private::InstructionType inst_type) override {
+    return SupportsEmulatingInstructionsOfTypeStatic(inst_type);
+  }
+
+  bool ReadInstruction() override;
+
+  bool EvaluateInstruction(uint32_t evaluate_options) override;
+
+  bool TestEmulation(lldb_private::Stream *out_stream,
+                     lldb_private::ArchSpec &arch,
+                     lldb_private::OptionValueDictionary *test_data) override {
+    return false;
+  }
+
+  bool GetRegisterInfo(lldb::RegisterKind reg_kind, uint32_t reg_num,
+                       lldb_private::RegisterInfo &reg_info) override;
+
+  bool
+  CreateFunctionEntryUnwind(lldb_private::UnwindPlan &unwind_plan) override;
+
+private:
+  typedef struct {
+    uint32_t mask;
+    uint32_t value;
+    bool (EmulateInstructionPPC64::*callback)(const uint32_t opcode);
+    const char *name;
+  } Opcode;
+
+  Opcode *GetOpcodeForInstruction(const uint32_t opcode);
+
+  bool EmulateMFSPR(const uint32_t opcode);
+  bool EmulateSTD(const uint32_t opcode);
+  bool EmulateOR(const uint32_t opcode);
+  bool EmulateADDI(const uint32_t opcode);
+};
+
+#endif // EmulateInstructionPPC64_h_
